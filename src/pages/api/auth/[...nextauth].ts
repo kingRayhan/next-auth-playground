98c2e83ccfc151a938d46b7d76b3f6f55b2f66c9
King Rayhan <rayhan095@gmail.com>
2022-10-18 06:51:13 +0600

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,9 +9,8 @@ export const authOptions = {
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId:
-        "1028124840646-belq8vdq0tvnii7n32s797ivduu8b11i.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
     }),
   ],
 };
